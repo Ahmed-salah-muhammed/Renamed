0d1d56a6e0ee79b01b8bca502fb1cbb0226e4896
Ahmed Salah Abd El-bari <142721848+Ahmed-salah-muhammed@users.noreply.github.com>
2025-05-28 16:56:01 +0300

    renamed

---
+++ remove_name.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+folder_path = rf"pathhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+
+if not os.path.exists(folder_path):
+    print(f" المسار غير موجود: {folder_path}")
+    exit()
+
+pattern = re.compile(r"what you want to delete")
+
+for filename in os.listdir(folder_path):
+    old_path = os.path.join(folder_path, filename)
+
+    if os.path.isfile(old_path):
+        new_name = pattern.sub("", filename).strip()  
+        new_path = os.path.join(folder_path, new_name)
+
+        if filename != new_name:
+            try:
+                os.rename(old_path, new_path)
+                print(f" تمت إعادة التسمية: {filename} → {new_name}")
+            except Exception as e:
+                print(f" خطأ أثناء إعادة التسمية {filename}: {e}")
+
+print(" Done ")
